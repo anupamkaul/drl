c750e4b90914a64cb60bbc4239fa5391c01d3f2c
Anupam Kaul <anupam.kaul@yahoo.com>
2023-11-19 19:35:02 -0800

    Create valid random maps(frozen-lake), interpret actions, print steps
    Dump misc enviroments into test7
    
    Readup gym's core.py to understand Wrappers, ObservationWrapper, ActionWrapper, RewardWrapper
    Study different wrappers in the wrapper dir. Study video_recorder wrapper and monitoring folder
    (monitor class is deprecated but not the video recording functionality)

--- simenvs/base/test7_misc.py
+++ simenvs/base/test7_misc.py
@@ -3,7 +3,6 @@ import gymnasium as gym
 # https://gymnasium.farama.org/environments/mujoco/humanoid_standup/
 
 # -- play with environments (look at source of env in gymnasium to learn more)
-
 #env = gym.make("FrozenLake", render_mode="human") # default map_name = 4x4, is_slippery=True
 #env = gym.make("FrozenLake", map_name="8x8", render_mode="human") # double the map size to 8 by 8
 #env = gym.make("FrozenLake", map_name="8x8", is_slippery=False, render_mode="human") # non-slippery mode
@@ -12,24 +11,42 @@ import gymnasium as gym
 from gym.envs.toy_text.frozen_lake import generate_random_map
 env = gym.make("FrozenLake", desc = generate_random_map(8), map_name="8x8",  is_slippery=False, render_mode="human") # generate a random map each time
 
+#env = gym.make("Acrobot", render_mode="human")
+
+#env = gym.make("MountainCar", render_mode="human")
+
+#env = gym.make("Pendulum", render_mode="human")
+
+#env = gym.make("CliffWalking", render_mode="human")
+
+#env = gym.make("Pusher", render_mode="human")
+
+#env = gym.make("Reacher", render_mode="human")
+
+#env = gym.make("Walker2d", render_mode="human")
+
+#env = gym.make("HalfCheetah", render_mode="human")
+
+#env = gym.make("Hopper", render_mode="human")
+
+#env = gym.make("Ant", render_mode="human")
+
+#env = gym.make("BipedalWalker", render_mode="human")
+
+#env = gym.make("CarRacing", render_mode="human")
+
+#env = gym.make("Blackjack", render_mode="human")
+
+#env = gym.make("Taxi", render_mode="human")
+
+
 observation, info = env.reset(seed=42)
 
 import time
 
-def translate(action: int) -> str:
-	if action==0:
-		 return "LEFT"
-	elif action==1:
-		 return "DOWN"
-	elif action==2:
-		 return "RIGHT"
-	elif action==3:
-		 return "UP"
-
-
 for _ in range(1000):
 	action = env.action_space.sample() # (take action) this is where I will insert my policy (sample for now)
-	print (_, " next action : ", action, " (", translate(action), ")\n")
+	print (_, " next action : ", action, "\n")
 	time.sleep(1) # add a 1 second delay in steps to observe the next action...
 
 	observation, reward, terminated, truncated, info = env.step(action) # (observe the env-state, reward) 
