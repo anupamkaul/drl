e78edbd5020ca855f3bc84d49be7ffbc703c2709
Anupam Kaul <anupam.kaul@yahoo.com>
2023-11-19 20:20:01 -0800

    Show explore vs exploit by wrapper RandomizedActionWrapper
    env is now wrapped in RandomizedActionWrapper. This subclasses ActionWrapper
    and reimplements action to take random action at epsilon (default is 0.1) to
    go truly random. Of course it assumes that its dealing with a learned policy
    for the agent. (The agent currently itself is stochastic so its randomizing a
    random behavior already but at least it shows how I can break the sampling from
    a learnt policy (action) that the agent takes and how I can intermingle it using
    a probability function to explore by being truly random to the learnt policy.

--- simenvs/base/test2_cartpole_wrapper.py
+++ simenvs/base/test2_cartpole_wrapper.py
@@ -11,8 +11,26 @@ import gymnasium as gym
 # explore other wrappers from Gymnasium (inherit some of the predefined wrappers and extend them
 # to achieve additional functionality
 
-env = gym.make("CartPole-v1", render_mode="human")
-#env = gym.wrappers.Monitor(env, "recording") #deprecated
+import random
+import time
+
+class RandomizedActionWrapper(gym.ActionWrapper):
+	
+	def __init__(self, env, epsilon=0.1):
+		super(RandomizedActionWrapper, self).__init__(env)  # initialize parent class (ActionWrapper's) env
+		self.epsilon = epsilon
+
+	def action(self, action):
+		if random.random() < self.epsilon:
+			print("Random Action (Explore)!")
+			time.sleep(2)
+			return self.env.action_space.sample()
+		print("Use the action per policy (Exploit)!")
+		return action
+
+env = RandomizedActionWrapper(gym.make("CartPole-v1", render_mode="human"))
+
+# from here onwards our wrapper is the env (not the original env)
 
 total_reward=0
 observation, info = env.reset(seed=42)
@@ -21,7 +39,9 @@ import time #only to visually see end of episode
 
 for _ in range(1000):
 
-    action = env.action_space.sample()
+    action = env.action_space.sample() # hacked here because of the override by wrapper RandomizedActionWrapper
+    # gets the action from env but actually goes via our wrapper (its another thing as to why action is part of env, but leave that aside for now)
+
     print("action: ", action, "\n")
 
     observation, reward, terminated, truncated, info = env.step(action)
