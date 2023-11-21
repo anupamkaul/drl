91e784774159b1636c215e53156082e195bfe27c
Anupam Kaul <anupam.kaul@yahoo.com>
2023-11-20 21:06:47 -0800

    Code up Gymnasium-Robotics FetchRobot simulations with RL
    Uses FetchRobotics' robot for fetch, reach, slide and pick-n-place operations.
    Uses mujoco as the rendering backend. Episodes are default at 50 and can be overridden.
    Uses the actual goal, desired goal concepts.
    
    This introduces new environments to exploit DRL and make these tasks
    reach their goals in the shortest possible times.

---
+++ simenvs/base/gymnasium_robotics/fetchenv/fetchpicknplace.py
@@ -0,0 +1,49 @@
+import gymnasium as gym
+
+# code: https://github.com/Farama-Foundation/Gymnasium-Robotics
+# doc : https://robotics.farama.org/envs/fetch/pick_and_place/
+
+# Fetch has to pick up a box from a table using its gripper and move it to a desired goal above the table.
+# Fetch simulates fetchrobotics.com 
+
+env = gym.make("FetchPickAndPlace-v2", max_episode_steps=150, render_mode="human")
+env.reset()
+obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+# The following always has to hold:
+assert reward == env.compute_reward(obs["achieved_goal"], obs["desired_goal"], info)
+assert truncated == env.compute_truncated(obs["achieved_goal"], obs["desired_goal"], info)
+assert terminated == env.compute_terminated(obs["achieved_goal"], obs["desired_goal"], info)
+
+# However goals can also be substituted:
+substitute_goal = obs["achieved_goal"].copy()
+substitute_reward = env.compute_reward(obs["achieved_goal"], substitute_goal, info)
+substitute_terminated = env.compute_terminated(obs["achieved_goal"], substitute_goal, info)
+substitute_truncated = env.compute_truncated(obs["achieved_goal"], substitute_goal, info)
+
+# loop to observe states and send back random actions
+
+total_reward=0
+observation, info = env.reset(seed=42)
+
+import time #only to visually see end of episode
+
+for _ in range(1000):
+
+    action = env.action_space.sample()
+    print(_, " action: ", action, "\n")
+
+    observation, reward, terminated, truncated, info = env.step(action)
+    total_reward += reward
+    print("observation: ", observation, "\n reward: ", reward, "total reward: ", total_reward, "info: ", info, "\n")
+	
+
+    if terminated or truncated:
+        print("end of episode\n")
+        #input() -- this makes render window go BG
+        time.sleep(2)
+        total_reward=0
+        observation, info = env.reset()
+
+env.close()
+
