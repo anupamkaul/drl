aee022d211cb99b9a420797b8d643927f9afe18f
Anupam Kaul <anupam.kaul@yahoo.com>
2023-11-26 23:54:09 -0800

    Add plotting using TensorBoard (summarywriter)
    Save run files and view in browser (http://localhost:6006)
    Code works and I can inspect sin, cos function growth in realtime
    
    I had protobuf incompatiblity errors initially but pip install tensorflow
    fixed it. Run command: tensorboard --logdir runs --host localhost

--- simenvs/base/dl_pytorch/example1.py
+++ simenvs/base/dl_pytorch/example1.py
@@ -5,4 +5,23 @@ import numpy as np
 a = torch.FloatTensor(3, 2)
 print(a)
 
+# no nvidia driver: cannot allocate GPU mem to tensor yet
+#ca = a.cuda()
+#print(ca)
+
+import math
+from tensorboardX import SummaryWriter
+
+writer = SummaryWriter()
+funcs = {"sin":math.sin, "cos":math.cos, "tan":math.tan} 
+
+for angle in range(-360, 360):
+	angle_rad = angle * math.pi / 180
+	for name, fun in funcs.items():
+		val = fun(angle_rad)
+		writer.add_scalar(name, val, angle)
+	writer.close()
+
+
+
 
