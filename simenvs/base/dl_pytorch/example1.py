b7862eb795e0c66a5a695832a148b7f32ed2286e
Anupam Kaul <anupam.kaul@yahoo.com>
2023-12-25 15:37:58 -0800

    Add example1.py and tensorboard code for general pytorch usage
    This is from my linux laptop (unstaged commits)

--- simenvs/base/dl_pytorch/example1.py
+++ simenvs/base/dl_pytorch/example1.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 
+# 1. Creating tensors
+
 # create an uninitialized tensor of 3 x 2
 a = torch.FloatTensor(3, 2)
 print(a)
@@ -9,18 +11,36 @@ print(a)
 #ca = a.cuda()
 #print(ca)
 
+# clear the tensor content
+a.zero_()
+print(a)
+
+# _ is inplace call. Operates on tensor content and returns
+# same object/mem. Functional creates copy. 
+
+# create using python iterable
+b = torch.FloatTensor([1,2,3], [3,2,1]) 
+print(b)
+
+# create a zero object using numpy first
+n = np.zeros(shape = (3, 2))
+print(n)
+b = torch.tensor(n)
+print(b)
+
+
 import math
 from tensorboardX import SummaryWriter
 
 writer = SummaryWriter()
-funcs = {"sin":math.sin, "cos":math.cos, "tan":math.tan} 
+funcs = {"sin": math.sin, "cos": math.cos, "tan": math.tan} 
 
 for angle in range(-360, 360):
 	angle_rad = angle * math.pi / 180
 	for name, fun in funcs.items():
 		val = fun(angle_rad)
 		writer.add_scalar(name, val, angle)
-	writer.close()
+writer.close()
 
 
 
