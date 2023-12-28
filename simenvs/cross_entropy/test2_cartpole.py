535e6ae57efd1fbd9a72c019f9d07c3abc95f173
Anupam <anupam.kaul@yahoo.com>
2023-12-27 17:21:50 -0800

    Code up the cross entropy RL algorithm for cartpole
    Corrected errors (referencing pytorch variables)
    Checking in so as to preserve my work
    
    Latest error is:
    python3 test2_cartpole.py
    /Users/anupkaul/akaul_git/drl/simenvs/cross_entropy/test2_cartpole.py:109: UserWarning: Creating a tensor from a list of numpy.ndarrays is extremely slow. Please consider converting the list to a single numpy.ndarray with numpy.array() before converting to a tensor. (Triggered internally at /Users/runner/work/pytorch/pytorch/pytorch/torch/csrc/utils/tensor_new.cpp:264.)
      obs_v = torch.FloatTensor([obs])
    Traceback (most recent call last):
      File "/Users/anupkaul/akaul_git/drl/simenvs/cross_entropy/test2_cartpole.py", line 175, in <module>
        for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
      File "/Users/anupkaul/akaul_git/drl/simenvs/cross_entropy/test2_cartpole.py", line 109, in iterate_batches
        obs_v = torch.FloatTensor([obs])
    ValueError: expected sequence of length 4 at dim 2 (got 0)

--- simenvs/cross_entropy/test2_cartpole.py
+++ simenvs/cross_entropy/test2_cartpole.py
@@ -34,8 +34,10 @@ randomly, as the method is robust and converges very quickly.
 
 '''
 # https://gymnasium.farama.org/environments/classic_control/cart_pole/
+
 env = gym.make("CartPole-v1", render_mode="human")
 #env = gym.wrappers.Monitor(env, "recording")
+
 observation, info = env.reset(seed=42)
 
 HIDDEN_SIZE = 128 # single layer count of neurons
@@ -47,7 +49,7 @@ PERCENTILE  = 70  # percentile of episodes' total rewards that we use for elite 
 class Net (nn.Module):
 	def __init__(self, obs_size, hidden_size, n_actions):
 		super(Net, self).__init__()
-		self.net = nn.sequential(
+		self.net = nn.Sequential(
 			nn.Linear(obs_size, hidden_size),
 			nn.ReLU(),
 			nn.Linear(hidden_size, n_actions)
@@ -55,8 +57,147 @@ class Net (nn.Module):
 	def forward(self, x):
 		return self.net(x)
 
+'''
+Above is a pretty simple network: it takes a single observation from the environment as an input vector
+and outputs a number for every action that we can perform. The output from the network is a probability
+distribution over actions. Instead of including the traditional softmax non-linearity after the last step,
+(which uses exponentiation) and then calculating cross-entropy loss (which uses logarithm of probabilities)
+we'll use the pytorch class nn.CrossEntropyLoss which combines softmax and cross-entropy in a single, more
+numerically stable expression. 
+
+Now let's define Episode and EpisodeStep. These are two helper classes:
+
+EpisodeStep: This will be used to represent one single step (action) that our agent (the ML now) made in
+the episode. EpisodeStep stores the observation from the environment and what action the agent completed.
+(We will use episode steps from "elite" episode steps as training data for our ML algorithm.
+
+Episode: This is a single episode itself, stored as total undiscounted reward and a collection of EpisodeStep
+'''
+
+from collections import namedtuple
+
+Episode = namedtuple('Episode', field_names=['reward', 'steps'])  # remember steps are EpisodeSteps
+EpisodeStep = namedtuple('EpisodeStep', field_names=['observation', 'action']) 
+
+'''
+Now let's define a function that generates batches with episodes
+'''
+
+def iterate_batches(env, net, batch_size):
+	batch = []
+	episode_reward = 0.0
+	episode_steps = []
+	obs = env.reset()
+	sm = nn.Softmax(dim=1)
+
+	'''
+	The above function accepts the environment (the Env class instance from Gymnasium library), our neural network,
+	and the count of episodes it should generate on every iteration. We have the notion of a list of Episode instances
+	that we call a batch. This list of Episodes will be captured by the variable 'batch' above (which is a list of Episode
+	instances). We also declare a reward counter for the current episode and its list of steps(the EpisodeStep objects). 
+
+	Then we reset the env to obtain the first observation and create a softmax layer, which will be used to convert the
+	network's output to a probability distribution (between 0 and 1) of actions. 
+
+	That was the prep work, now let's start the environment loop:
+
+	'''
+
+	import torch
+
+	while True:
+		obs_v = torch.FloatTensor([obs])
+		act_probs_v = sm(net(obs_v))     #apply softmax to the neural network (agent) that takes in 1 observation
+		act_probs = act_probs_v.data.numpy()[0]
+	
+		# explanation of above
+
+		action = np.random.choice(len(act_probs), p = act_probs)
+		next_obs, reward, is_done, _ = env.step(action)
+
+		# write some more
+
+		episode_reward += reward
+		episode_steps.append(EpisodeStep(observation=obs, action=action))
+
+		if is_done:
+			batch.append(Episode(reward=episode_reward, steps = episode_steps))	
+			episode_reward = 0.0
+			episode_steps = []
+			next_obs = env.reset()
+
+			#exit loop
+			if len(batch) == batch_size:
+				yield batch
+
+				batch = []
+				obs = next_obs
 
 
+def filter_batch(batch, percentile):
+
+	rewards = list(map(lambda s: s.reward, batch))
+	reward_bound = np.percentile(rewards, percentile)
+	reward_mean = float(np.mean(rewards))
+
+	#explanations galore!
+
+	train_obs = []
+	train_act = []
+
+	for example in batch:
+		if example.reward < reward_bound:
+			continue
+		train_obs.extend(map(lambda step: step.action, example.steps))
+
+		#explanations
+
+	train_obs_v = torch.FloatTensor(train_obs)
+	train_act_v = torch.LongTensor(train_act)
+	return train_obs_v, train_act_v, reward_bound, reward_mean
+
+# main glue (continues from top)
+
+obs_size = env.observation_space.shape[0]
+n_actions = env.action_space.n
+
+net = Net(obs_size, HIDDEN_SIZE, n_actions)
+objective = nn.CrossEntropyLoss()
+
+from torch import optim
+optimizer = optim.Adam(params=net.parameters(), lr = 0.01)
+
+from tensorboardX import SummaryWriter
+writer = SummaryWriter()
+
+#explanations
+
+for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
+	obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
+	optimizer.zero_grad()
+	action_scores_v = net(obs_v)
+	loss_v = objective(action_scores_v, acts_v)
+	loss_v.backward()
+	optimizer.step()
+
+	# explanations
+
+	print("%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f" % (iter_no, loss_v.item(), reward_m, reward<b))
+	writer.add_scalar("loss", loss_v.item(), iter_no)
+	writer.add_scalar("reward_bound", reward_b, iter_no)
+	writer.add_scalar("reward_mean",  reward_m, iter_no)
+
+	# explanations
+
+	if reward_m > 199:
+		print("RL Solved !!\n")
+		break
+
+	writer.close()
+
+
+'''
+# original code
 import time
 for _ in range(1000):
 	action = env.action_space.sample() # (take action) this is where I will insert my policy (sample for now)
@@ -71,6 +212,7 @@ for _ in range(1000):
 		observation, info = env.reset()
 
 env.close()
+'''
 
 
 
