9f9063b3561b4e33837523c751072acf8c31ae10
Anupam <anupam.kaul@yahoo.com>
2023-12-31 12:21:28 -0800

    Save and load example model
    loads and prints tensors associated with the model
    runs eval() but I haven't verified the output
    optimizer not loaded yet

--- simenvs/base/dl_pytorch/example_loadsave.py
+++ simenvs/base/dl_pytorch/example_loadsave.py
@@ -39,3 +39,33 @@ for var_name in optimizer.state_dict():
     print(var_name, "\t", optimizer.state_dict()[var_name])
 
 
+# saving and loading the model for inference:
+
+# save:
+PATH = "./models/mymodel"
+
+import torch
+torch.save(model.state_dict(), PATH)
+print("Model saved to ", PATH)
+
+# reload:
+
+#modelnew = TheModelClass(*args, **kwargs) # general
+
+print("Model Loaded from ", PATH)
+modelnew = TheModelClass()
+modelnew.load_state_dict(torch.load(PATH))
+modelnew.eval()
+
+# Print model's state_dict
+print("Model's state_dict:")
+for param_tensor in modelnew.state_dict():
+    print(param_tensor, "\t", modelnew.state_dict()[param_tensor].size())
+
+'''
+# Print optimizer's state_dict
+print("Optimizer's state_dict:")
+for var_name in optimizer.state_dict():
+    print(var_name, "\t", optimizer.state_dict()[var_name])
+'''
+
