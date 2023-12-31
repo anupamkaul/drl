64aec958e0a49f1baf36a4ce42089e9bdd37c163
Anupam <anupam.kaul@yahoo.com>
2023-12-30 18:27:14 -0800

    Add car racing, cliff walking and taxi environments
    Tested that basic obs/acns/rewards work. For some reason
    taxi on my macbook doesn't render.
    
    Also renamed lunar and added a gym.envs.txt that greps for all
    envs I could find in gymnasium

---
+++ simenvs/base/toy_text/test_taxi.py
@@ -0,0 +1,23 @@
+import gymnasium as gym
+
+env = gym.make("Taxi-v3", render_mode="human")
+observation, info = env.reset(seed=42)
+
+import time
+for _ in range(1000):
+	action = env.action_space.sample() # (take action) this is where I will insert my policy (sample for now)
+	print (_, " action : ", action, "\n")
+
+	observation, reward, terminated, truncated, info = env.step(action) # (observe the env-state, reward) 
+	print ("observation : ", observation, "\nreward : ", reward, "\n")
+
+	if terminated or truncated:
+		print(" oops .. terminated or truncated !!\n")
+		time.sleep(2)
+		observation, info = env.reset()
+
+env.close()
+
+
+
+
